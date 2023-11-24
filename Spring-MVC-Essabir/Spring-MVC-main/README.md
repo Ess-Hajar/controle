0af420aea8aeca59dc26bbd77290ad470885644e
Hajar <130552361+Ess-Hajar@users.noreply.github.com>
2023-11-24 15:36:13 +0100

    Add files via upload

---
+++ Spring-MVC-Essabir/Spring-MVC-main/README.md
@@ -0,0 +1,25 @@
+
+<h1>Conception</h1>
+<img src="captures/Conception.png">
+<h1>Authentification</h1>
+<img src="captures/Authentification.png">
+<h1>L'interface Admin</h1>
+<img src="captures/admin_interface.png">
+<h1>Supprimer un patient de la liste</h1>
+<img src="captures/DELETE.png">
+<h1>Suppression avec succées</h1>
+<img src="captures/DELETE_Succees.png">
+<h1>Chercher un patient</h1>
+<img src="captures/Chercher_un_patient.png">
+<h1>Ajouter un patient</h1>
+<img src="captures/Ajouter_patient1.png">
+<img src="captures/Ajouter_patient2.png">
+<h1>Modufier les informations d'un patient</h1>
+<img src="captures/Modification.png">
+<img src="captures/Modification2.png">
+<h2>Modification avec succées</h2>
+<img src="captures/Modification3.png">
+<h1>Interface utilisateur</h1>
+<img src="captures/User_interface.png">
+<h2> si un utilisateur tente d'accéder à une ressource pour laquelle il n'a pas les autorisations nécessaires, il sera redirigé vers la page suivante :</h2>
+<img src="captures/Role_error.png">
---
+++ Spring-MVC-Essabir/Spring-MVC-main/src/main/java/com/mvc/patientspringmvc/PatientSpringMvcApplication.java
@@ -0,0 +1,83 @@
+package com.mvc.patientspringmvc;
+
+import com.mvc.patientspringmvc.entities.Patient;
+import com.mvc.patientspringmvc.repositories.PatientRepository;
+import com.mvc.patientspringmvc.security.service.AccountService;
+import jakarta.validation.constraints.Null;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import java.util.Date;
+
+@SpringBootApplication //class principal de l'application
+public class PatientSpringMvcApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PatientSpringMvcApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(new Patient(null, "Hajar", new Date(), false, 11));
+            patientRepository.save(new Patient(null, "Nada",new Date(),true,250));
+            patientRepository.save(new Patient(null, "Oussama",new Date(),true,665));
+            patientRepository.save(new Patient(null, "Ikhlas",new Date(),false,34));
+            patientRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+        };
+    }
+    //@Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+        PasswordEncoder passwordEncoder=passwordEncoder();
+        return args -> {
+            UserDetails u1= jdbcUserDetailsManager.loadUserByUsername("user11");
+            UserDetails u2= jdbcUserDetailsManager.loadUserByUsername("user22");
+            UserDetails u3= jdbcUserDetailsManager.loadUserByUsername("admin2");
+            if (u1== null)
+                jdbcUserDetailsManager.createUser(
+                        User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build()
+                );
+            if (u2== null)
+                jdbcUserDetailsManager.createUser(
+                        User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build()
+                );
+            if (u3== null)
+                jdbcUserDetailsManager.createUser(
+                        User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
+                );
+        };
+    }
+
+    //@Bean
+    CommandLineRunner commandLineRunnerUserDetails(AccountService accountService){
+        return args -> {
+            accountService.addNewRole("USER");
+            accountService.addNewRole("ADMIN");
+            accountService.addNewUser("user1","1234","user1@gmail.com","1234");
+            accountService.addNewUser("user2","1234","user2@gmail.com","1234");
+            accountService.addNewUser("admin","1234","admin@gmail.com","1234");
+
+            accountService.addRoleToUser("user1","USER");
+            accountService.addRoleToUser("user2","USER");
+            accountService.addRoleToUser("admin","USER");
+            accountService.addRoleToUser("admin","ADMIN");
+
+
+
+        };
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
